0c2cec8730e35991f75c268d92bec1da8a18354d
gustavooarantes <dev.gustavoa@gmail.com>
2025-12-14 14:36:15 -0300

    feat: added hot reloading to simplify development and environment variables proper handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/gustavooarantes/Blog/internal/env"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/gustavooarantes/Blog
 
 go 1.25.4
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
